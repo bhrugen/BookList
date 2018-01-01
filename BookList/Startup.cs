a5c99568e7cab0405324aafc9085b6d8381be2ed
Ben Spark <bhrugen1990@gmail.com>
2018-01-01 16:09:09 -0600

    Add Book Model and Add to Db

--- BookList/Startup.cs
+++ BookList/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using BookList.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookList
 {
@@ -21,6 +23,8 @@ namespace BookList
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddMvc();
         }
 

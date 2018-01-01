cf168a520dfbd333821485ea9135b65bd8b4613d
Ben Spark <bhrugen1990@gmail.com>
2018-01-01 16:17:28 -0600

    BookController : Create Action

--- BookList/Controllers/BooksController.cs
+++ BookList/Controllers/BooksController.cs
@@ -21,6 +21,25 @@ namespace BookList.Controllers
             return View(_db.Books.ToList());
         }
 
+        //Get : Book/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Add(book);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(book);
+        }
+
         protected override void Dispose(bool disposing)
         {
             if (disposing)
--- BookList/Migrations/20180101221653_updateNameProperty.Designer.cs
+++ BookList/Migrations/20180101221653_updateNameProperty.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace BookList.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180101221653_updateNameProperty")]
+    partial class updateNameProperty
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
